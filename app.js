086df37670e64c3779edb9346a8afa55dc753122
avakrishn <avakrishn@gmail.com>
2019-04-25 11:25:01 -0700

    condense code

--- app.js
+++ app.js
@@ -1,38 +1,23 @@
-
-
+'use strict';
 
 window.onload = () => {
-
   createKeys(); // init function
 }
 
-createKeys = () =>{
+const createKeys = () =>{
    const keys = {'A': 'CLAP', 'S': 'HIHAT', 'D' : 'KICK', 'F': 'OPENHAT', 'G': 'BOOM', 'H': 'RIDE', 'J' : 'SNARE', 'K': 'TOM', 'L': 'TINK'};
 
-   let keysDiv = document.createElement('div');
-   keysDiv.classList.add('keys');
+   const keysDiv = domElementCreation('div', null, 'keys');
 
    for(let key in keys){
-      const newKey = domElementCreation('div', null, 'key',key.charCodeAt(0));
+      const newKey = domElementCreation('div', null, 'key', key.charCodeAt(0));
       const keyLetter = domElementCreation('kbd', key);
       const keySound = domElementCreation('span', keys[key], 'sound');
-
-      // const newKey = document.createElement('div');
-      // newKey.classList.add('key');
-      // newKey.setAttribute('data-key', key.charCodeAt(0));
-
-      // const keyLetter = document.createElement('kbd');
-      // keyLetter.innerHTML = key;
-
-      // const keySound = document.createElement('span');
-      // keySound.classList.add('sound')
-      // keySound.innerHTML = keys[key];
-
 
       newKey.append(keyLetter, keySound);
       keysDiv.appendChild(newKey);
    }
-   console.log(keysDiv);
+   
    document.getElementById('root').appendChild(keysDiv);
 
    function domElementCreation(tagName, text, className, dataKey){
