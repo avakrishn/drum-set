a794f4c9e9c02571a2bc8ad119c30d1865e4a14e
avakrishn <avakrishn@gmail.com>
2019-04-26 16:25:19 -0700

    import keys and sounds from module

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
 'use strict';
+import {keys , sounds} from './data.mjs';
 
 window.onload = () => {
   createKeys(); // init function
 }
 
 const createKeys = () =>{
-   const keys = {'A': 'CLAP', 'S': 'HIHAT', 'D' : 'KICK', 'F': 'OPENHAT', 'G': 'BOOM', 'H': 'RIDE', 'J' : 'SNARE', 'K': 'TOM', 'L': 'TINK'};
-
    const keysDiv = domElementCreation('div', null, 'keys');
+   const audioDiv = document.getElementById('audio');
+   const root = document.getElementById('root');
 
    for(let key in keys){
       const newKey = domElementCreation('div', null, 'key', key.charCodeAt(0));
       const keyLetter = domElementCreation('kbd', key);
-      const keySound = domElementCreation('span', keys[key], 'sound');
+      const keySoundSpan = domElementCreation('span', keys[key], 'sound');
+      const audio = domElementCreation('audio', null, null, key.charCodeAt(0), sounds[keys[key]]);
 
-      newKey.append(keyLetter, keySound);
+      newKey.append(keyLetter, keySoundSpan);
       keysDiv.appendChild(newKey);
+      audioDiv.appendChild(audio);
    }
    
-   document.getElementById('root').appendChild(keysDiv);
+   root.appendChild(keysDiv);
 
-   function domElementCreation(tagName, text, className, dataKey){
+   function domElementCreation(tagName, text, className, dataKey, src){
       const newElement = document.createElement(tagName);
-      text ? newElement.innerText = text : null; 
+      text ? newElement.innerText = text.toUpperCase()  : null; 
       className ? newElement.classList.add(className) : null;
       dataKey ? newElement.setAttribute('data-key', dataKey) : null;
+      src ? newElement.setAttribute('src', src) : null;
+      // newElement.addEventListener('click', keyClicked);
       return newElement;
    }
+
+   // function keyClicked(event){
+
+   // }
 
 }
