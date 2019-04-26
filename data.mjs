3e0df6f68d30fca1bfd96adb0578168a43385962
avakrishn <avakrishn@gmail.com>
2019-04-26 16:24:58 -0700

    export data for keys and sounds

---
+++ data.mjs
@@ -0,0 +1,27 @@
+'use strict';
+
+export const sounds = {
+   'clap': "https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/clap.wav", 
+   'hihat': "https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/hihat.wav", 
+   'kick': "https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/kick.wav", 
+   'openhat': "https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/openhat.wav", 
+   'boom': "https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/boom.wav", 
+   'ride': "https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/ride.wav", 
+   'snare': "https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/snare.wav", 
+   'tom': "https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/tom.wav", 
+   'tink' : "https://raw.githubusercontent.com/wesbos/JavaScript30/master/01%20-%20JavaScript%20Drum%20Kit/sounds/tink.wav"
+};
+
+export const keys = {
+   'A': 'clap', 
+   'S': 'hihat', 
+   'D' : 'kick', 
+   'F': 'openhat', 
+   'G': 'boom', 
+   'H': 'ride', 
+   'J' : 'snare', 
+   'K': 'tom', 
+   'L': 'tink'
+};
+
+
