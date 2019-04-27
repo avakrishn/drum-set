9744b014215c1f40ca3fb22f414b971980c7e6fc
avakrishn <avakrishn@gmail.com>
2019-04-26 17:44:24 -0700

    add playSound and removeTransition functions to keys

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ window.onload = () => {
 }
 
 const createKeys = () =>{
-   const keysDiv = domElementCreation('div', null, 'keys');
-   const audioDiv = document.getElementById('audio');
    const root = document.getElementById('root');
+   const audioDiv = document.getElementById('audio');
+   const keysDiv = domElementCreation('div', null, 'keys');
 
    for(let key in keys){
       const newKey = domElementCreation('div', null, 'key', key.charCodeAt(0));
@@ -16,12 +16,16 @@ const createKeys = () =>{
       const keySoundSpan = domElementCreation('span', keys[key], 'sound');
       const audio = domElementCreation('audio', null, null, key.charCodeAt(0), sounds[keys[key]]);
 
+      newKey.addEventListener('click', playSound);
+      newKey.addEventListener('transitionend', removeTransition);
+
       newKey.append(keyLetter, keySoundSpan);
       keysDiv.appendChild(newKey);
       audioDiv.appendChild(audio);
    }
    
    root.appendChild(keysDiv);
+   window.addEventListener('keydown', playSound); 
 
    function domElementCreation(tagName, text, className, dataKey, src){
       const newElement = document.createElement(tagName);
@@ -29,12 +33,30 @@ const createKeys = () =>{
       className ? newElement.classList.add(className) : null;
       dataKey ? newElement.setAttribute('data-key', dataKey) : null;
       src ? newElement.setAttribute('src', src) : null;
-      // newElement.addEventListener('click', keyClicked);
       return newElement;
    }
 
-   // function keyClicked(event){
+   function playSound(event){
+      const keyCode = (event.keyCode) ? event.keyCode : this.getAttribute('data-key');
 
-   // }
+      const audioKey = document.querySelector(`audio[data-key="${keyCode}"]`);
+
+      let key = document.querySelector(`.key[data-key="${keyCode}"]`);;
+      // if key is clicked on screen  
+      if(this !== window) key = this;
+      
+      if(!audioKey) return;
+
+      audioKey.currentTime = 0; // rewind to the start
+      audioKey.play();
+
+      key.classList.add('playing');
+   }
+
+   function removeTransition(event){
+      if(event.propertyName !== "transform") return;
+
+      this.classList.remove('playing');
+   }
 
 }
